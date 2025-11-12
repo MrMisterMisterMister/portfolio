47a3d3d28360754e4ba041a8c32a626e67d18122
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-11-12 23:45:19 +0100

    Update deploy.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mrmistermistermister.github.io/portfolio',
+  site: 'https://mrmistermistermister.github.io',
+  base: '/portfolio/',
   integrations: [
     react(),
     sitemap(),
